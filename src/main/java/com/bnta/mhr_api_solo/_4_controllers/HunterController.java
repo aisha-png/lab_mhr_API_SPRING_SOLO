b7f4c051abada0db7db05776edcd80708b6a0e80
Aisha Mohamed <aisha_mo@hotmail.com>
2022-05-18 20:01:00 +0100

    can show route

--- src/main/java/com/bnta/mhr_api_solo/_4_controllers/HunterController.java
+++ src/main/java/com/bnta/mhr_api_solo/_4_controllers/HunterController.java
@@ -6,15 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("hunters")
 public class HunterController {
-
 
     @Autowired
     HunterRepository hunterRepository;
@@ -22,6 +23,12 @@ public class HunterController {
     @GetMapping //localhost:8080/hunters
     public ResponseEntity<List<Hunter>> getHunters(){
         return new ResponseEntity<>(hunterRepository.findAll(), HttpStatus.OK);
+    }
+
+//    TODO: SHOW ROUTE
+    @GetMapping("/{id}")//localhost:8080/hunters/1
+    public ResponseEntity<Optional<Hunter>> getHunter(@PathVariable Long id){
+        return new ResponseEntity<>(hunterRepository.findById(id), HttpStatus.OK);
     }
 
 
