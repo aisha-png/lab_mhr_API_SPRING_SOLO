5734391f22bd295d6f95380f479cd0901912fbf9
Aisha Mohamed <aisha_mo@hotmail.com>
2022-05-18 15:11:40 +0100

    first commit

---
+++ src/main/java/com/bnta/mhr_api_solo/MhrApiSoloApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.mhr_api_solo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhrApiSoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhrApiSoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnta/mhr_api_solo/MhrApiSoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.mhr_api_solo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhrApiSoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
