e7f1058d46f6cb20858ec68f12307895a1779db0
Aisha Mohamed <aisha_mo@hotmail.com>
2022-05-18 20:21:43 +0100

    Can create/post

--- src/main/java/com/bnta/mhr_api_solo/_4_controllers/HunterController.java
+++ src/main/java/com/bnta/mhr_api_solo/_4_controllers/HunterController.java
@@ -5,10 +5,7 @@ import com.bnta.mhr_api_solo._2_repositories.HunterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,15 +17,23 @@ public class HunterController {
     @Autowired
     HunterRepository hunterRepository;
 
+//    TODO: INDEX
     @GetMapping //localhost:8080/hunters
     public ResponseEntity<List<Hunter>> getHunters(){
         return new ResponseEntity<>(hunterRepository.findAll(), HttpStatus.OK);
     }
 
 //    TODO: SHOW ROUTE
-    @GetMapping("/{id}")//localhost:8080/hunters/1
+    @GetMapping("/{id}")//localhost:8080/hunters/10
     public ResponseEntity<Optional<Hunter>> getHunter(@PathVariable Long id){
         return new ResponseEntity<>(hunterRepository.findById(id), HttpStatus.OK);
+    }
+
+//    TODO: CREATE/POST
+    @PostMapping
+    public ResponseEntity<Hunter> createHunter(@RequestBody Hunter newHunter){
+        hunterRepository.save(newHunter);
+        return new ResponseEntity<>(newHunter, HttpStatus.OK);
     }
 
 
