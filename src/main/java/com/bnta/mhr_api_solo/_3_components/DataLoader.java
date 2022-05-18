90bb6f9a9f7afb40e198b76b2d6f908b1df07b09
Aisha Mohamed <aisha_mo@hotmail.com>
2022-05-18 17:28:03 +0100

    added hunters to Loader

--- src/main/java/com/bnta/mhr_api_solo/_3_components/DataLoader.java
+++ src/main/java/com/bnta/mhr_api_solo/_3_components/DataLoader.java
@@ -9,6 +9,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 public class DataLoader implements ApplicationRunner {
 
@@ -25,7 +27,18 @@ public class DataLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 //        TODO: ADD SOME HUNTERS:
-//        Hunter hunter1 = new Hunter("")
+        Hunter hunter1 = new Hunter("Mad Dog", "Long Sword", "PALAMUTE");
+        Hunter hunter2 = new Hunter("Scar", "Dual Blade", "PALAMUTE");
+        Hunter hunter3 = new Hunter("Gunner", "Light BowGun", "PALICO");
+        Hunter hunter4 = new Hunter("Zero", "bow", "PALAMUTE");
+        Hunter hunter5 = new Hunter("Iron Heart", "Charge Blade", "PALICO");
+        Hunter hunter6 = new Hunter("Slasher", "Great Sword", "PALICO");
+        Hunter hunter7 = new Hunter("Diablo", "Heavy BowGun", "PALAMUTE");
+        Hunter hunter8 = new Hunter("Sythe", "Switch Axe", "PALICOE");
+        Hunter hunter9 = new Hunter("Insurgent", "Gunlance", "PALAMUTE");
+        Hunter hunter10 = new Hunter("Skull Crusher", "Hammer", "PALAMUTE");
+        hunterRepository.saveAll(Arrays.asList
+                (hunter1, hunter2, hunter3, hunter4, hunter5, hunter6, hunter7, hunter8, hunter9, hunter10));
 
     }
 }
