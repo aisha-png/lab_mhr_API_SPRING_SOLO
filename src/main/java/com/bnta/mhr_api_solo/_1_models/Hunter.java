3916b75465acded2b524d811f1f1b1ec50f59d62
Aisha Mohamed <aisha_mo@hotmail.com>
2022-05-18 15:53:53 +0100

    Hunter annotations & toString

--- src/main/java/com/bnta/mhr_api_solo/_1_models/Hunter.java
+++ src/main/java/com/bnta/mhr_api_solo/_1_models/Hunter.java
@@ -1,18 +1,27 @@
 package com.bnta.mhr_api_solo._1_models;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "hunters")
 public class Hunter {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String name;
 
+    @Column
     private String weapon;
 
+    @Column
     private String buddyType;
 
+    @ManyToMany(mappedBy = "hunters")
     private List<Quest> quests;
 
 //    DEFAULT CONSTRUCTOR:
@@ -63,5 +72,24 @@ public class Hunter {
 
     public void setQuests(List<Quest> quests) {
         this.quests = quests;
+    }
+
+    public void addQuest(Quest quest){
+        this.quests.add(quest);
+    }
+
+    public void removeQuest(Quest quest){
+        this.quests.remove(quest);
+    }
+
+    @Override
+    public String toString() {
+        return "Hunter{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", weapon='" + weapon + '\'' +
+                ", buddyType='" + buddyType + '\'' +
+                ", quests=" + quests +
+                '}';
     }
 }
